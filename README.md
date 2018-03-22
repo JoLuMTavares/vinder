2bd764074bb47d55a1edfcf209210e7207c7a245
JoLuMTavares <joaoluism.tavares@gmail.com>
2018-03-22 09:59:37 +0100

    *.*

--- README.md
+++
@@ -1,7 +0,0 @@
-# Starter Web 
-
-## Introduction
-
-## Technology
-
- 
---
+++ scripts/hallo.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+echo 'Hallo!'
+
+echo 'We have the TDF. Now is the Pista coming.'
+
+echo "Who knows what will come next."
+
+echo "Best V12 engine from this planet. Best V8 engine from this planet."
+
+echo "How wonderful would be a V16?"
